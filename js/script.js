d2c4b1bdb112692bb60ebdcb98a3708dbbce1748
Sam Medriano <sam.medriano@outlook.com>
2021-10-07 09:27:07 +0800

    Remove old files and replaced with updated codes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Account {
+  constructor(firstName, lastName, email, password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
+}
