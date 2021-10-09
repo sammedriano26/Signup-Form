34a0c84e7d158b2c6386f5d5074afc99b3a2a683
Sam Medriano <sam.medriano@outlook.com>
2021-10-10 03:42:32 +0800

    Add JS rules

---
+++ README.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - Intro component with sign up form solution
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See hover states for interactive elements
+
+### Links
+
+- Solution URL: https://github.com/sammedriano26/Signup-Form
+- Live Site URL: https://sammedriano26.github.io/Signup-Form/
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Clean code using SASS
+- Vanilla JavaScript
+
+### What I learned
+
+HTML and CSS are not new to me at this point; however, JavaScript is what I find myself struggling for now. Gladly, this project made me feel pretty confident that I am on the right path of learning how to code.
+
+Form Authentication is one of the most common task in JavaScript. Knowing how to build one inside and out is very crucial to fully understanding JavaScript. On this task, I learned how to:
+
+- Create event listeners and attach them to a form.
+- Create a function that allows me to validate user inputs.
+- RegEx - to validate special rules in JS such as in the case of Email and Password.
+- How to add classes to the HTML markup.
+- How to use the logical ! NOT operator to simplify and present my code in a cleaner manner.
--- js/script.js
+++ js/script.js
@@ -1,10 +1,93 @@
 "use strict";
 
-class Account {
-  constructor(firstName, lastName, email, password) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
+const form = document.getElementById("form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+// Listen to submit event on the form
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  inputValidator();
+});
+
+function inputValidator() {
+  // Retrieve input values | Use trim() to remove any extra spaces.
+  const firstNameVal = firstName.value.trim();
+  const lastNameVal = lastName.value.trim();
+  const emailVal = email.value.trim();
+  const passwordVal = password.value.trim();
+
+  // Check First Name
+  if (!firstNameVal) {
+    deletePlaceholder(firstName);
+
+    setErrorFor(firstName, "First Name cannot be empty.");
+  } else {
+    // Add Success Class
+    setSuccessFor(firstName);
   }
+
+  // Check Last Name
+  if (!lastNameVal) {
+    deletePlaceholder(lastName);
+    setErrorFor(lastName, "Last Name cannot be empty.");
+  } else {
+    setSuccessFor(lastName);
+  }
+
+  // Check Email
+  if (!emailVal) {
+    deletePlaceholder(email);
+    setErrorFor(email, "Email cannot be empty");
+  } else if (!validateEmail(emailVal)) {
+    email.style.color = "#fe7678";
+
+    setErrorFor(email, "Looks like this is not an email.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  // Check Password
+  if (!passwordVal) {
+    deletePlaceholder(password);
+    setErrorFor(password, "Password cannot be empty.");
+  } else {
+    setSuccessFor(password);
+  }
+}
+
+// Error function
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; // .form-control
+  const errorMsg = formControl.querySelector(".error-msg");
+
+  // Add error message inside errorMsg
+  errorMsg.innerText = message;
+
+  // Add error class
+  formControl.className = "form-control error";
+}
+
+// Success function
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Add success class
+  formControl.className = "form-control success";
+}
+
+// Validate email
+function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(email).toLowerCase());
+}
+
+// Delete placeholder
+function deletePlaceholder(input) {
+  input.placeholder = "";
 }
